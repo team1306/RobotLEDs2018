a2af53e77d72f4aca1d710b418311e025dd3ea75
Sam Roquitte <samroq@yahoo.com>
2018-04-04 00:48:41 -0500

    Its a double rainbow!!!

--- RobotLEDs2018.ino
+++ RobotLEDs2018.ino
@@ -5,8 +5,11 @@
 #define BRIGHTNESS  64
 #define LED_TYPE    NEOPIXEL
 
+const int TYPE_SWITCH_PIN = 12;
+const int ALLIANCE_PIN = 10;
+
 // Comment out for prod to save memory
-#define DEBUG 1
+//#define DEBUG 1
 
 CRGB leds[NUM_LEDS];
 
@@ -20,11 +23,27 @@ void setup() {
   #endif
   FastLED.addLeds<LED_TYPE, LED_PIN>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
+  setAll(CRGB(0,0,0));
+  pinMode(TYPE_SWITCH_PIN, INPUT);
+  pinMode(ALLIANCE_PIN, INPUT);
 }
 
 
 void loop() {
-  rainbowSingle(35);
+  if (digitalRead(TYPE_SWITCH_PIN)) {
+//    rainbowSingle(35);
+    rainbowDouble(35);
+  } else {
+    setAll(CRGB(0,0,0));
+    if (digitalRead(ALLIANCE_PIN)) {
+      // Blue
+      nightRiderSides(CRGB(0, 0, 255), CRGB(0,0,0), 3, 10, 0);
+    } else {
+      // Red
+      nightRiderSides(CRGB(255, 0, 0), CRGB(0,0,0), 3, 10, 0);
+    }
+//    nightRider(CRGB(255, 0, 0), CRGB(0, 0, 0), 3, 50);
+  }
 //  rainbowDouble(35);
 //  nightRider(CRGB(255, 0, 0), CRGB(255, 255, 255), 3, 50);
 //  nightRiderSides(CRGB(255, 0, 0), CRGB(0, 0, 0), 3, 1000);
@@ -34,6 +53,19 @@ void loop() {
 void setAll(CRGB c) {
   for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = c;
+  }
+  FastLED.show();
+}
+
+void setDoubleSide(int i, CRGB c) {
+  if (i < 23) {
+    // Bottom
+    leds[22-i] = c;
+    leds[23+i] = c;
+  } else if (i < 71) {
+    // Top
+    leds[142-i+22] = c;
+    leds[46+i-23] = c;
   }
   FastLED.show();
 }
@@ -57,72 +89,39 @@ void nightRiderSidesOneWay(CRGB c, CRGB c2, int len, int delayTime) {
     delay(delayTime);
   }
 }
-void nightRiderSidesOneWayOpp(CRGB c, CRGB c2, int len, int delayTime) {
-  int i=0;
-  for (int i=0; i < NUM_LEDS/2; i++) {  // Forward
-    leds[i] = c;
-    leds[(NUM_LEDS/2) + i] = c;
-    if (i - len >= 0) {
-      leds[i - len] = c2;
-      leds[((NUM_LEDS/2) + i) - len] = c2;
-    }
-    FastLED.show();
-    delay(delayTime);
-  }
-  for (int i = len; i >= 0; i--) {
-    leds[NUM_LEDS - i] = c2;
-    leds[(NUM_LEDS/2) - i] = c2;
-    FastLED.show();
-    delay(delayTime);
-  }
-}
-void nightRiderSides(CRGB c, CRGB c2, int len, int delayTime) {
-//  int i=0;
-//  for (int i=0; i < NUM_LEDS/2; i++) {  // Forward
-//    leds[i] = c;
-//    leds[(NUM_LEDS/2) + i] = c;
-//    if (i - len >= 0) {
-//      leds[i - len] = c2;
-//      leds[((NUM_LEDS/2) + i) - len] = c2;
-//    }
-//    FastLED.show();
-//    delay(delayTime);
-//  }
-//  for (int i = len; i >= 0; i--) {
-//    leds[NUM_LEDS - i] = c2;
-//    leds[(NUM_LEDS/2) - i] = c2;
-//    FastLED.show();
-//    delay(delayTime);
-//  }
 
-  int i=0;
-  for (int i=(NUM_LEDS/2); i > 0; i--) {  // Forward
-    leds[i] = c;
-    leds[(NUM_LEDS/2) + i] = c;
-    if (i - len >= 0) {
-      leds[i - len] = c2;
-      leds[((NUM_LEDS/2) + i) - len] = c2;
+/**
+ * Nightrider effect
+ * CRGB c - color
+ * CRGB c2 - background color
+ * int len - length
+ * int delayTime - delay time
+ * int dir - direction
+ *  0 = both ways
+ *  1 = up
+ *  2 = down
+ */
+void nightRiderSides(CRGB c, CRGB c2, int len, int delayTime, int dir) {
+  if (dir == 0 || dir == 1) {
+    // Up
+    for (int i = 0; i < NUM_LEDS/2; i++) {
+      setDoubleSide(i, c);
+      if (i > len-1) {
+        setDoubleSide(i-len, c2);
+      }
+      delay(delayTime);
     }
-    FastLED.show();
-    delay(delayTime);
   }
-  for (int i = 0; i < len; i++) {
-    leds[NUM_LEDS - i] = c2;
-    leds[(NUM_LEDS/2) - i] = c2;
-    FastLED.show();
-    delay(delayTime);
+  if (dir == 0 || dir == 2) {
+    // Down
+    for (int i = 0; i < NUM_LEDS/2; i++) {
+      setDoubleSide((NUM_LEDS/2) - i, c);
+      if ((NUM_LEDS/2) - i > len-1) {
+        setDoubleSide((NUM_LEDS/2) - i + len, c2);
+      }
+      delay(delayTime);
+    }
   }
-  
-//  for (int i=0; i < NUM_LEDS/2; i++) {  // Backward
-//    leds[NUM_LEDS - i] = c;
-//    leds[(NUM_LEDS/2) - i] = c;
-//    if ((NUM_LEDS-i) <= NUM_LEDS-len) {
-//      leds[(NUM_LEDS-i) + len] = c2;
-//      leds[((NUM_LEDS/2)-i) + len] = c2;
-//    }
-//    FastLED.show();
-//    delay(delayTime);
-//  }
 }
 
 void rainbowSingle(uint8_t wait) {
@@ -142,10 +141,7 @@ void rainbowDouble(uint8_t wait) {
 
   for(j=0; j<256; j++) {
     for(i=0; i<NUM_LEDS/2; i++) {
-      leds[i] = Wheel((i+j) & 255);
-    }
-    for(i=(NUM_LEDS/2); i<NUM_LEDS; i++) {
-      leds[i] = Wheel((i+j - (NUM_LEDS/2)) & 255);
+      setDoubleSide(i, Wheel((i+j) & 255));
     }
     FastLED.show();
     delay(wait);
